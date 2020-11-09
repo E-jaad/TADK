db641e2c41a90ecd4c043f3c0996ec5fb52ada10
salman <salman.khalid@prettylittlething.com>
2020-11-09 16:10:03 +0500

    rt example

--- exampleRT.java
+++ exampleRT.java
@@ -2,12 +2,35 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import TADK.*;
 public class exampleRT {
+
+    public final static String ip = "127.0.0.1";
+    public final static int port = 4815;    
+    public final static int rtAddress = 1;
     public static void main(String args[]) throws UnknownHostException, IOException, FailedToConfigure
 	{ 
-        short[] data = new short[32];
+        short[] data1 = new short[]{1,2,3,4,5};
+        short[] data2 = new short[]{1,2,3,4,5,6,7,8,9,10};
+        short[] data3 = new short[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         //Create an object of RT and connect to TADK through tcp on the give ip and port and set RT address as well
-        RemoteTerminal rt = new RemoteTerminal("192.168.0.102",9761,13);
-        //Set 32 words in Transmit dataset of RT 13 at SubAdress 3
-        rt.writeSubAddress(3, data);                
+        RemoteTerminal rt = new RemoteTerminal(ip,port,rtAddress);
+        
+        rt.writeSubAddress(1, data1);         
+        rt.writeSubAddress(2, data2);
+        rt.writeSubAddress(3, data3);
+
+        Thread rtRecieveThread = new Thread(()->{
+            System.out.println("Recieve Thread Started");
+            while(true){
+                DeviceData recData;
+				try {
+                    recData = rt.readDeviceData();
+                    System.out.println("Recieved [" + recData.subAddress+ "] :"+recData.data);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+            }
+        }, "RTReciever");
+
+        rtRecieveThread.start();
     }
 }
