1547920e93ccd482db73a65578a7609fdd365b04
sadiqamin2 <43720108+sadiqamin2@users.noreply.github.com>
2021-01-01 16:36:34 +0500

    Sdk 001 add doc (#4)
    * sdk-001-add-busmonitor
    
    * sdk-001-add-doc

--- exampleBM.java
+++ exampleBM.java
@@ -23,7 +23,9 @@ public class exampleBM {
 	{ 
         //Create an object of BM and connect to TADK through tcp on the given ip and port and configure TADK as BM
         BusMonitor bm = new BusMonitor(ip, port);
-        bm.setTrap(13, 1, 1, 7);
+        System.out.println("Connected to " + ip + ":" + port);
+        bm.disableBM();
+        bm.setTrap(13, 1, DeviceConstants.TADK_BC_TO_RT, 7);
         bm.enableBM();
         Thread rtRecieveThread = new Thread(()->{
             System.out.println("Recieve Thread Started");
@@ -32,6 +34,7 @@ public class exampleBM {
 				try {
                     recData = bm.readDeviceData();
                     System.out.println("Recieved [" + recData.subAddress+ "] :"+recData.response);
+                    System.out.println("Data[0]: " + recData.data[0]);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
